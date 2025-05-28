6642fc2a7cd044df2f28526adea6c5a19ec9f5d8
Kosta Rainesh <kosta.rainesh@overwolf.com>
2025-05-28 21:21:55 +0300

    added faker

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import {test, expect} from '@playwright/test'
 import { PageManager } from '../page-objects/pageManager'
+import {faker} from '@faker-js/faker'
 
 test.beforeEach(async({page}) => {
     await page.goto('http://localhost:4200/')
@@ -16,13 +17,15 @@ test('navigate to form page', async({page}) => {
 
 test('parametrized methods', async ({page}) => {
     const pm = new PageManager(page)
+    const randomFullName = faker.person.fullName()
+    const randomEmail = `${randomFullName.replace(' ', '')}${faker.number.int(1000)}@test.com`
 
     await pm.navigateTo().formLayoutsPage()
     await pm.onFormLayoutsPage().submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', 'welcome1', 'Option 2')
-    await pm.onFormLayoutsPage().submitInlineFormWithNameAndEmailAndCheckbox('Kosta rainesh', 'test@test.com', false)
-    await pm.navigateTo().datePickerPage()
-    await pm.onDatepickerPage().selectCommonDatepickerDateFromToday(5)
-    await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(6, 15)
+    await pm.onFormLayoutsPage().submitInlineFormWithNameAndEmailAndCheckbox(randomFullName, randomEmail, false)
+    // await pm.navigateTo().datePickerPage()
+    // await pm.onDatepickerPage().selectCommonDatepickerDateFromToday(5)
+    // await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(6, 15)
 })
 
 
