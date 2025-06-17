7d2ed69791bb95dfc4969bb85b1df6599d09c822
Kosta Rainesh <kosta.rainesh@overwolf.com>
2025-06-17 14:40:45 +0300

    added new test

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -31,6 +31,11 @@ test('parametrized methods', async ({page}) => {
     await pm.onDatepickerPage().selectCommonDatepickerDateFromToday(5)
     await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(6, 15)
 })
+test.only('testing with argos ci', async({page}) => {
+    const pm = new PageManager(page)
+    await pm.navigateTo().formLayoutsPage()
+    await pm.navigateTo().datePickerPage()
+})
 
 
 
